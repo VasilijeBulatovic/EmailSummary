c27da83c384597aead191a4f077a691d3232e9ad
Polovni racunari <polovniracunari3@gmail.com>
2025-05-17 15:02:58 +0200

    Added file to test ssh configuration

---
+++ prepare.py
@@ -0,0 +1,30 @@
+print("Prepare emails for summarization, make sure to run this script before main.py")
+print("\nInitializing Email Summary Tool...\n")
+
+# ASCII Art of a laptop/computer
+art = """
+     ,----------------,              
+     ,-----------------,             
+     |  .-----------.  |             
+     |  |           |  |             
+     |  | >_        |  |             
+     |  |           |  |             
+     |  |           |  |             
+     |  `-----------'  |             
+     |      Email      |             
+     |     Summary     |             
+     `-----------------|             
+      |||||||||||||||||             
+      |||||||||||||||||
+      |||||||||||||||||
+     /  \/\/\/\/\/\/  \\
+    /    \/\/\/\/\/    \\
+   /      \/\/\/\/      \\
+  /        \/\/\/        \\
+ /          \/\/          \\
+/            \/            \\
+`---------------------------'
+"""
+
+print(art)
+print("\nReady to process emails...")
